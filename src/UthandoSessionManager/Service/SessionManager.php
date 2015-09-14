20f24e93369184355d9b2ab0943d03e72b7b2184
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-09-14 22:52:53 +0100

    finished service tests

--- src/UthandoSessionManager/Service/SessionManager.php
+++ src/UthandoSessionManager/Service/SessionManager.php
@@ -1,13 +1,27 @@
 <?php
+/**
+ * Uthando CMS (http://www.shaunfreeman.co.uk/)
+ *
+ * @package   UthandoSessionManager\Service
+ * @author    Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @link      https://github.com/uthando-cms for the canonical source repository
+ * @copyright Copyright (c) 2014 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license   see LICENSE.txt
+ */
 
 namespace UthandoSessionManager\Service;
 
 use UthandoCommon\Service\AbstractMapperService;
 
+/**
+ * Class SessionManager
+ *
+ * @package UthandoSessionManager\Service
+ */
 class SessionManager extends AbstractMapperService
 {	
     protected $serviceAlias = 'UthandoSessionManager';
-    
+
     public function getById($id, $col = null)
     {
         $id = (string) $id;
---
+++ test/UthandoSessionManagerTest/Service/SessionManagerTest.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Uthando CMS (http://www.shaunfreeman.co.uk/)
+ *
+ * @package   UthandoSessionManagerTest\Service
+ * @author    Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @copyright Copyright (c) 2014 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license   see LICENSE.txt
+ */
+
+namespace UthandoSessionManagerTest\Service;
+
+use UthandoSessionManager\Model\Session;
+use UthandoSessionManagerTest\Bootstrap;
+
+class SessionManagerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var \Zend\ServiceManager\ServiceManager
+     */
+    protected $serviceManager;
+
+    protected $traceError = true;
+
+    public function setUp()
+    {
+        $this->serviceManager = Bootstrap::getServiceManager();
+    }
+
+    public function testGetById()
+    {
+        $model = new Session();
+        $model->setId('2b73fftuqqa84lha6jt4pcl097')
+            ->setName('uthando-cms')
+            ->setModified('1398807915')
+            ->setLifetime(1440)
+            ->setData('');
+
+        $mapperMock = $this->getMockBuilder('UthandoSessionManager\Mapper\Session')
+            ->disableOriginalConstructor()
+            ->getMock();
+        $mapperManagerMock = $this->getMockBuilder('UthandoCommon\Mapper\MapperManager')
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mapperMock->expects($this->once())
+            ->method('getById')
+            ->with('2b73fftuqqa84lha6jt4pcl097')
+            ->will($this->returnValue($model));
+
+        $mapperManagerMock->expects($this->once())
+            ->method('get')
+            ->with('UthandoSessionManager')
+            ->will($this->returnValue($mapperMock));
+
+        $this->serviceManager->setAllowOverride(true);
+        $this->serviceManager->setService('UthandoMapperManager', $mapperManagerMock);
+
+        $service = $this->serviceManager->get('UthandoServiceManager')
+            ->get('UthandoSessionManager');
+
+        $this->assertSame($model, $service->getById('2b73fftuqqa84lha6jt4pcl097'));
+    }
+}
