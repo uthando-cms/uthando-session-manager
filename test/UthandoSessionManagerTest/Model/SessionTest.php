1952e3ccd3fd36707e261ed3b1a06048d18afe8d
shaunfreeman <shaun@shaunfreeman.co.uk>
2015-09-15 12:10:54 +0100

    added new tests

---
+++ test/UthandoSessionManagerTest/Model/SessionTest.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Uthando CMS (http://www.shaunfreeman.co.uk/)
+ *
+ * @package   UthandoSessionManagerTest\Model
+ * @author    Shaun Freeman <shaun@shaunfreeman.co.uk>
+ * @copyright Copyright (c) 2014 Shaun Freeman. (http://www.shaunfreeman.co.uk)
+ * @license   see LICENSE.txt
+ */
+
+namespace UthandoSessionManagerTest\Model;
+
+use UthandoSessionManager\Model\Session as SessionModel;
+use UthandoSessionManagerTest\Bootstrap;
+
+class SessionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var SessionModel
+     */
+    protected $model;
+
+    /**
+     * @var \Zend\ServiceManager\ServiceManager
+     */
+    protected $serviceManager;
+
+    public function setUp()
+    {
+        $this->serviceManager = Bootstrap::getServiceManager();
+        $this->model = new SessionModel();
+    }
+
+    public function testSetGetId()
+    {
+        $this->model->setId('2b73fftuqqa84lha6jt4pcl097');
+        $this->assertSame('2b73fftuqqa84lha6jt4pcl097', $this->model->getId());
+    }
+
+    public function testSetGetName()
+    {
+        $this->model->setName('uthando-cms');
+        $this->assertSame('uthando-cms', $this->model->getName());
+    }
+
+    public function testSetGetModified()
+    {
+        $this->model->setModified('1398807915');
+        $this->assertSame(1398807915, $this->model->getModified());
+    }
+
+    public function testSetGetLifetime()
+    {
+        $this->model->setLifetime('1440');
+        $this->assertSame(1440, $this->model->getLifetime());
+    }
+
+    public function testSetGetData()
+    {
+        $this->model->setData('some data');
+        $this->assertSame('some data', $this->model->getData());
+    }
+}
